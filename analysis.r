6559b7a327dae53182aa078b88564d443fa07fd0
stevelan <stevelan1995@gmail.com>
2023-08-23 21:25:07 +0800

    completed training data visualization

--- analysis.r
+++ analysis.r
@@ -1,5 +1,5 @@
 # Predictive analysis of the loan approval dataset
-# We will build a predictive model to help our company to improve the efficiency and accuracy of home loan approval. 
+# We will build a predictive model to help our company to improve the efficiency and accuracy of home loan approval.
 
 # Dataset Description:
 # Variable	Description
@@ -29,7 +29,7 @@ library(car)
 
 setwd("~/Desktop/MBA/Analytics for Managers/final_project")
 loan <- read.csv("loan_approval_dataset.csv")
-# View(loan)
+View(loan)
 # summary(loan)
 
 # Create a scatterplot matrix using ggpairs function
@@ -42,10 +42,37 @@ set.seed(3451)
 #     p = c(train = 0.6, valid = 0.3, test = 0.1))
 # str(inds)
 split1 <- sample.split(loan$loan_status, SplitRatio = 0.7)
-train_data <- iris[split1, ]
-temp_data <- iris[!split1, ]
+train_data <- loan[split1, ]
+temp_data <- loan[!split1, ]
 
 # Splitting the temporary set into validation (15%) and test (15%)
 split2 <- sample.split(temp_data$loan_status, SplitRatio = 0.5)
 validation_data <- temp_data[split2, ]
 test_data <- temp_data[!split2, ]
+
+categorical_columns <- c("education", "self_employed")
+numerical_columns <- c(
+    "no_of_dependents", "income_annum", "loan_amount",
+    "loan_term", "cibil_score", "residential_assets_value",
+    "commercial_assets_value", "luxury_assets_value",
+    "bank_asset_value")
+
+# numerical_columns <- c(
+#     "no_of_dependents", "income_annum", "loan_amount",
+#     "loan_term", "cibil_score", "residential_asset_value",
+#     "commercial_assets_value", "luxury_assets_value",
+#     "bank_asset_value"
+# )
+
+
+# visualize the training set and see what we can find between features and the target variable
+# Create pairwise histogram
+# categorical_cols_to_visualize <- c(c("loan_status"), categorical_columns)
+# categorical_var_plot <- ggpairs(train_data, columns = categorical_cols_to_visualize)
+# ggsave("cat_var_hist.png", categorical_var_plot, 'png', './')
+
+numerical_cols_to_visualize <- c(c("loan_status"), numerical_columns)
+num_var_plot <- ggpairs(train_data, columns = numerical_cols_to_visualize)
+num_var_plot
+ggsave("num_var_plot.png", num_var_plot, 'png', './')
+# train_data$numerical_columns
